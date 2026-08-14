fc0256bed83d305431b8d7f5b009131bede8fe74
sevincyunusova <yunusovasevinc08@gmail.com>
2026-08-15 03:02:28 +0400

    feat: add responsive mobile navigation

--- cinevault/src/components/Navbar/Navbar.jsx
+++ cinevault/src/components/Navbar/Navbar.jsx
@@ -1,24 +1,51 @@
-import { FaHeart } from "react-icons/fa"
+import { useState } from "react"
+import { FaHeart, FaBars, FaTimes } from "react-icons/fa"
 
 import "./Navbar.css"
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const closeMenu = () => {
+    setMenuOpen(false)
+  }
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
 
-        <a href="#top" className="navbar-logo">
+        <a
+          href="#top"
+          className="navbar-logo"
+          onClick={closeMenu}
+        >
           CineVault
         </a>
 
-        <div className="navbar-links">
-          <a href="#movies">
+        <button
+          className="menu-toggle"
+          onClick={() => setMenuOpen(!menuOpen)}
+          aria-label="Toggle navigation menu"
+        >
+          {menuOpen ? <FaTimes /> : <FaBars />}
+        </button>
+
+        <div
+          className={`navbar-links ${
+            menuOpen ? "mobile-open" : ""
+          }`}
+        >
+          <a
+            href="#movies"
+            onClick={closeMenu}
+          >
             Movies
           </a>
 
           <a
             href="#favorites"
             className="favorites-link"
+            onClick={closeMenu}
           >
             <FaHeart />
             <span>Favorites</span>
